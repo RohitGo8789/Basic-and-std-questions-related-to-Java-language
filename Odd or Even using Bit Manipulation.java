80d2985308858306e50b140cf8118b543f4ea46f
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-04-09 20:58:26 +0530

    Check if a given number is odd or even using bitmask

---
+++ Odd or Even using Bit Manipulation.java
@@ -0,0 +1,16 @@
+public class OddOrEvenUsingBits {
+    public static void OddorEven(int n){
+        int bitMask = 1;
+        if((n & bitMask) == 0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    public static void main(String args[]){
+        OddorEven(2);
+        OddorEven(5);
+        OddorEven(0);
+
+    }
+}
