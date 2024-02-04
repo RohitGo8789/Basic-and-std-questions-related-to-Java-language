e6a281d9710f5128e505e0ca9f6de831dfbd3311
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2024-02-04 23:29:22 +0530

    Find index number of array

--- Find index number of array.java
+++ Find index number of array.java
@@ -1,4 +1,4 @@
-public class ArrayAssignment{
+public class ArrayAssignment {
     public static int findIndex(int nums[],int target){
         for(int i = 0; i <nums.length;i++){
             if(nums[i]==target){
